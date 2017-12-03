ee967034415ad3966b3a60633843f987a6f37c4a
Michael Kaiser <michael.kaiser@student.uibk.ac.at>
2017-12-03 15:10:34 +0100

    move everything in token class

--- main.py
+++ main.py
@@ -4,37 +4,10 @@ import math
 from my_token import Token
 import pickle
 
-addresses = []
-tokens = []
+contract_address = "0x9a642d6b3368ddc662CA244bAdf32cDA716005BC" #QTUM
 
-url = "https://ethplorer.io/service/service.php?refresh=holders&data=0x9a642d6b3368ddc662CA244bAdf32cDA716005BC&page=tab%3Dtab-holders%26pageSize%3D100%26holders%1D3"  # change to whatever your url is
-
-data = requests.get(url)
-data = data.json()
-records = data["pager"]["holders"]["records"]
-num_pages = math.ceil(records / 100)
-
-for i in range(1,num_pages):
-    print("processing page no. " + str(i) + " | " + str(num_pages))
-    url = "https://ethplorer.io/service/service.php?refresh=holders&data=0x9a642d6b3368ddc662CA244bAdf32cDA716005BC&page=tab%3Dtab-holders%26pageSize%3D100%26holders%3D" + str(i)
-    data = requests.get(url)
-    data = data.json()
-
-    for elem in data["holders"]:
-        addresses.append(elem["address"])
-        tokens.append(elem["balance"])
-
-txs_count = data["token"]["txsCount"]
-symbol = data["token"]["symbol"]
-transfers_count = data["token"]["transfersCount"]
-decimals = data["token"]["decimals"]
-holders_count = data["token"]["holdersCount"]
-total_supply = data["token"]["totalSupply"]
-
-data_tokenholders = {"address" : addresses, "tokens" : tokens}
-tokenholders = pd.DataFrame(data_tokenholders)
-
-t = Token(symbol, total_supply,decimals,holders_count, txs_count, transfers_count, tokenholders)
-pickle.dump(t, open("./data/qtum.p", "wb"))
-tokenholders.to_csv("./data/qtum.csv", sep = ",")
+t = Token(contract_address)
 print(t)
+
+t.dump("./data/qtum.p")
+t.token_distribution_to_csv("./data/qtum.csv")
